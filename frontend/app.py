c67a4a2c78d3352094281400735e415688c23be3
Your Name <your_email@example.com>
2026-09-25 00:50:17 +0000

    Upload all deployment files including large model via Git LFS

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 # Base URL of the Flask backend inside the Docker network
-BACKEND_URL = "https://didactic-pancake-xrppvq5jj54j36w55-5000.app.github.dev"
+BACKEND_URL = "https://curly-robot-xrppvq5jj5q6hp9wg-5000.app.github.dev"
 
 # Set page configuration and title
 st.set_page_config(page_title="SuperKart Sales Predictor", page_icon="🛒", layout="wide")
